8dd8cdefbfdf06c02cf7c6a4d705daab2fb1c3ae
FUNSHO <fejjy1998@gmail.com>
2022-07-19 23:16:12 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,28 +11,27 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i;
-	int swap, flag = 1;
-
-	if (!array || !size || size == 1)
-	{
-		return;
-	}
-	while (flag)
-	{
-		flag = 0;
-		for (i = 1; i < size; i++)
-		{
-			if (array[i - 1] > array[i])
-			{
-				swap = array[i - 1];
-				array[i - 1] = array[i];
-				array[i] = swap;
-				flag = 1;
-				print_array(array, size);
-			}
-		}
-		if (flag == 0)
-			break;
-	}
+unsigned int i;
+int swap, flag = 1;
+if (!array || !size || size == 1)
+{
+return;
+}
+while (flag)
+{
+flag = 0;
+for (i = 1; i < size; i++)
+{
+if (array[i - 1] > array[i])
+{
+swap = array[i - 1];
+array[i - 1] = array[i];
+array[i] = swap;
+flag = 1;
+print_array(array, size);
+}
+}
+if (flag == 0)
+break;
+}
 }
